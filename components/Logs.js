98566aee4f46aaf4a33cc650c27c55e119d4b87b
moonsunmi <celebmoon@naver.com>
2022-11-10 20:33:31 +0900

    시계와 버튼 컴포넌트 따로 빼냄

--- components/Logs.js
+++ components/Logs.js
@@ -9,12 +9,6 @@ const Logs = ({ logs }) => {
           <Log log={log} key={index} />
         ))}
       </div>
-      <div className=" bg-slate-100 text-slate-600 text-sm p-1 my-1 border border-slate-300 rounded-md">
-        2022년 11월 15일 AM 10:02 - AM 10:27
-      </div>
-      <div className=" bg-slate-100 text-slate-600 text-sm p-1 my-1 border border-slate-300 rounded-md">
-        2022년 11월 15일 AM 10:02 - AM 10:27
-      </div>
     </div>
   );
 };
--- components/TimerContainer.js
+++ components/TimerContainer.js
@@ -1,18 +1,22 @@
+import { useState } from "react";
 import Counter from "./Counter";
+import StartStopButton from "./StartStopButton";
 import State from "./State";
 
 const TimerContainer = ({ minutes, seconds, nextLogId, logs, addLogs }) => {
   // 분과 초를 따로 관리하는 게 나을 수도 있겠다.
+  const [isCounting, setIsCounting] = useState(false);
 
   return (
     <div className=" shadow-lg bg-yellow-100 text-yellow-600  text-lg items-center w-64 rounded-lg border border-yellow-600 p-3	">
       <div className=" border-yellow-600">리액트 프로그래밍 공부</div>
-      <Counter
-        minutes={minutes}
-        seconds={seconds}
+      <Counter minutes={minutes} seconds={seconds} isCounting={isCounting} />
+      <StartStopButton
         nextLogId={nextLogId}
         logs={logs}
         addLogs={addLogs}
+        isCounting={isCounting}
+        setIsCounting={setIsCounting}
       />
       <div className="border-t border-yellow-600 text-sm">
         <p className=" text-lg my-2">States</p>
